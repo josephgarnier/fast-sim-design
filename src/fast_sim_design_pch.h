0cdab6d95a9ca9e09aa760da1882dd1df7fe6ced
Joseph Garnier <garnjose@gmail.com>
2020-01-02 11:21:46 +0100

    Revert "update pch"
    This reverts commit aa4f02baf9988b5cc72efea034127f1de162dd29.

--- src/fast_sim_design_pch.h
+++ src/fast_sim_design_pch.h
@@ -6,19 +6,3 @@
 * LICENSE file in the root directory of this source tree.
 ******************************************************************************/
 
-#pragma once
-
-#ifndef FAST_SIM_DESIGN_FAST_SIM_DESIGN_PCH_H
-#define FAST_SIM_DESIGN_FAST_SIM_DESIGN_PCH_H
-
-#include "dll_config.h"
-
-#include "QtCore"
-#include "QtGlobal"
-#include "QDebug"
-#include "QGlobalStatic"
-
-/* #include "gui/ui_PlatformGui.h"
-#include "level/World.h"*/
-
-#endif
